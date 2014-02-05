d2de843190883b4fe9d2346399e436813f1112a2
antiline <antiline@jun2.org>
2014-02-05 13:35:53 +0900

    Support Ubuntu 12.04
    for Ubuntu Server 12.04

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,10 @@
 
 ############################################################################
 WEB_DIR="/var/services/web/comix-server"
-APACHE_CONF="/usr/syno/apache/conf/httpd.conf"
-PHP_CONF="/usr/syno/etc/php/user-setting.ini"
-APACHE_CMD="/usr/syno/apache/bin/httpd -DHAVE_PHP"
+APACHE_CONF="/etc/apache2/conf.d/aircomix.conf"
+APACHE_CMD="service apache2 restart"
 ############################################################################
- 
+
 
 echo "::::::::::::::::::::::::"
 echo "1. Remove comix-server"
@@ -16,43 +15,21 @@ if [ -f ${WEB_DIR} ]
 then
   rm -rf ${WEB_DIR}
 fi
- 
+
 
 echo "::::::::::::::::::::::::"
 echo "2. Undo Configure Apache"
 echo "::::::::::::::::::::::::"
 
-if [ -f ${APACHE_CONF}-comix ]
+if [ -f ${APACHE_CONF} ]
 then
-  rm -f ${APACHE_CONF}-comix
+  rm -f ${APACHE_CONF}
 fi
 
-if [ -f ${APACHE_CONF}-user.org ]
-then
-  mv -f ${APACHE_CONF}-user.org ${APACHE_CONF}-user
-fi
-
-if [ -f ${APACHE_CONF}.org ]
-then
-  mv -f ${APACHE_CONF}.org ${APACHE_CONF}
-fi
- 
 
 echo "::::::::::::::::::::::::"
-echo "3. Undo Configure PHP"
+echo "3. Restart Apache"
 echo "::::::::::::::::::::::::"
- 
-if [ -f ${PHP_CONF}.org ]
-then
-  mv -f ${PHP_CONF}.org ${PHP_CONF}
-fi
- 
 
-echo "::::::::::::::::::::::::"
-echo "4. Restart HTTPD"
-echo "::::::::::::::::::::::::"
- 
-kill `ps | grep HAVE_PHP | grep root | grep -v grep | awk '{print $1}'`
-sleep 5
 ${APACHE_CMD}
 
